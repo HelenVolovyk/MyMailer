10abbed3a1f133d763dc0979b7b3fc52acd30f6b
HelenVolovyk <74989529+HelenVolovyk@users.noreply.github.com>
2021-08-26 23:29:00 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,5 +3,11 @@
 require_once '../vendor/autoload.php';
 
 use Mailer\Transport\Message;
+/**
+ * @param string $subject;
+ * @param string $templateName;
+ * @param array $vars;
+ * @param string $to;
+ */
 
-Message::send( $subject, $templateName,  $vars, $to);
+Message::send($subject, $templateName, $vars, $to);
